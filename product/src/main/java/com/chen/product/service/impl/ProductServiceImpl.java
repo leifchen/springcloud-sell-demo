b5ab0dc617008ec04edb0ed83bdfc37d773a3a60
LeifChen <leifchen90@gmail.com>
2020-06-15 14:25:20 +0800

    [product]feat: add rabbitmq

--- product/src/main/java/com/chen/product/service/impl/ProductServiceImpl.java
+++ product/src/main/java/com/chen/product/service/impl/ProductServiceImpl.java
@@ -6,8 +6,10 @@ import com.chen.product.exception.ProductException;
 import com.chen.product.model.ProductInfo;
 import com.chen.product.repository.ProductInfoRepository;
 import com.chen.product.service.ProductService;
+import com.chen.util.JsonUtils;
 import com.chen.vo.DecreaseStockInput;
 import com.chen.vo.ProductInfoOutput;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,6 +31,8 @@ public class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ProductInfoRepository productInfoRepository;
+    @Autowired
+    private AmqpTemplate amqpTemplate;
 
     @Override
     public List<ProductInfo> findUpAll() {
@@ -47,23 +51,31 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    @Transactional(rollbackOn = ProductException.class)
     public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
-        decreaseStockProcess(decreaseStockInputList);
+        List<ProductInfo> productInfoList = decreaseStockProcess(decreaseStockInputList);
+
+        // 发送mq消息
+        List<ProductInfoOutput> productInfoOutputList = productInfoList.stream().map(e -> {
+            ProductInfoOutput output = new ProductInfoOutput();
+            BeanUtils.copyProperties(e, output);
+            return output;
+        }).collect(Collectors.toList());
+        amqpTemplate.convertAndSend("productInfo", JsonUtils.toJson(productInfoOutputList));
     }
 
-    @Transactional
     public List<ProductInfo> decreaseStockProcess(List<DecreaseStockInput> decreaseStockInputList) {
         List<ProductInfo> productInfoList = new ArrayList<>();
-        for (DecreaseStockInput decreaseStockInput: decreaseStockInputList) {
+        for (DecreaseStockInput decreaseStockInput : decreaseStockInputList) {
             Optional<ProductInfo> productInfoOptional = productInfoRepository.findById(decreaseStockInput.getProductId());
             // 判断商品是否存在
-            if (!productInfoOptional.isPresent()){
+            if (!productInfoOptional.isPresent()) {
                 throw new ProductException(ResultEnum.PRODUCT_NOT_EXIST);
             }
 
             ProductInfo productInfo = productInfoOptional.get();
             // 库存是否足够
-            Integer result = productInfo.getProductStock() - decreaseStockInput.getProductQuantity();
+            int result = productInfo.getProductStock() - decreaseStockInput.getProductQuantity();
             if (result < 0) {
                 throw new ProductException(ResultEnum.PRODUCT_STOCK_ERROR);
             }
