e660bdf48d7843ee03ede1002964c5910235deb3
LeifChen <leifchen90@gmail.com>
2020-06-16 10:07:48 +0800

    [sell-common]feat: add success & error method

--- sell-common/src/main/java/com/chen/common/Result.java
+++ sell-common/src/main/java/com/chen/common/Result.java
@@ -24,11 +24,25 @@ public class Result {
      */
     private Object data;
 
-    public static Result success(Object object) {
+    public static Result success() {
         Result result = new Result();
-        result.setData(object);
         result.setCode(0);
         result.setMsg("成功");
+        return result;
+    }
+    
+    public static Result success(Object object) {
+        Result result = new Result();
+        result.setCode(0);
+        result.setMsg("成功");
+        result.setData(object);
+        return result;
+    }
+
+    public static Result error(String msg) {
+        Result result = new Result();
+        result.setCode(1);
+        result.setMsg(msg);
         return result;
     }
 }
